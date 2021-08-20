08232a28e021d2cbe4cc79f9e6a358d66e64a18d
tenger2000 <xqdeng09@126.com>
2021-08-20 09:18:41 +0800

    完成菜单显示

---
+++ 自助订餐系统/menu.cpp
@@ -0,0 +1,44 @@
+#include "menu.h"
+
+Menu::Menu()
+{
+}
+
+Menu::Menu(string dishName, string dishID, double price)
+{
+}
+
+void Menu::operMenu()
+{
+	char buf[1024] = { '\0' };
+	ifstream ifs;
+	ifs.open("menu.txt", ios::in);
+	if (!ifs.is_open())
+	{
+		cout << "file not exist." << endl;
+		ifs.close();
+		ofstream fout("menu.txt");
+		if (fout)
+		{
+			cout << "请联系服务人员添加菜单" << endl;
+			system("pause");
+			return;
+		}
+	}
+	string ID, name, price;
+	cout << "编号" << setfill(' ') << setw(8) << "菜名" << setw(8) << "价格" << endl;
+	while (!ifs.eof())
+	{
+		ifs >> ID && ifs >> name && ifs >> price;
+		cout << left;
+		cout << setw(8) << ID << setw(8) << name << setw(8) << price << endl;
+	}
+}
+
+Date::Date()
+{
+}
+
+Date::Date(int y, int m, int d, int h)
+{
+}
